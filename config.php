5b97f3cea2896bd4afbbb308a7f5648a6218249d
FujiQwer <34383013+FujiQwer@users.noreply.github.com>
2018-01-31 02:03:55 +0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opt-kamorka.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opt-kamorka.ru/');
+
+// DIR
+define('DIR_APPLICATION', 'W:/domains/opt-kamorka.ru/catalog/');
+define('DIR_SYSTEM', 'W:/domains/opt-kamorka.ru/system/');
+define('DIR_IMAGE', 'W:/domains/opt-kamorka.ru/image/');
+define('DIR_LANGUAGE', 'W:/domains/opt-kamorka.ru/catalog/language/');
+define('DIR_TEMPLATE', 'W:/domains/opt-kamorka.ru/catalog/view/theme/');
+define('DIR_CONFIG', 'W:/domains/opt-kamorka.ru/system/config/');
+define('DIR_CACHE', 'W:/domains/opt-kamorka.ru/system/storage/cache/');
+define('DIR_DOWNLOAD', 'W:/domains/opt-kamorka.ru/system/storage/download/');
+define('DIR_LOGS', 'W:/domains/opt-kamorka.ru/system/storage/logs/');
+define('DIR_MODIFICATION', 'W:/domains/opt-kamorka.ru/system/storage/modification/');
+define('DIR_UPLOAD', 'W:/domains/opt-kamorka.ru/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'FujiQwer');
+define('DB_PASSWORD', 'papacloud3286');
+define('DB_DATABASE', 'fujiqwer');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
